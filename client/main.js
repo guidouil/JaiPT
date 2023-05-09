05bee8191f51c2cb8a520d7c2e358140ba63772a
Guillaume Darbonne <guidouil@gmail.com>
2023-05-09 09:52:20 +0200

    Theme and icons

--- client/main.js
+++ client/main.js
@@ -5,9 +5,8 @@ import "./main.html";
 const getRandomInt = () => Math.floor(Math.random() * 8) + 1;
 
 Template.home.events({
-  "click .jaiPtBtn"() {
+  "click #jaiPtBtn"() {
     const num = getRandomInt();
-    console.log(num);
     const audio = new Audio(`/snd/fart-0${num}.mp3`);
     audio.play();
   },

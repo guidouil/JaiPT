6b4e0ea5147cb207c663f5f6ebe1babce01be1a9
Guillaume Darbonne <guidouil@gmail.com>
2023-05-09 08:27:03 +0200

    Dark mode

--- client/main.js
+++ client/main.js
@@ -8,7 +8,40 @@ Template.home.events({
   "click .jaiPtBtn"() {
     const num = getRandomInt();
     console.log(num);
-    const audio = new Audio(`/fart-0${num}.mp3`);
+    const audio = new Audio(`/snd/fart-0${num}.mp3`);
     audio.play();
+  },
+});
+
+Template.darkMode.onRendered(() => {
+  // Dark mode detection
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    $("body").addClass("dark");
+  }
+});
+
+Template.darkMode.helpers({
+  isDark() {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      return true;
+    }
+    return false;
+  },
+});
+
+Template.darkMode.events({
+  "click #darkModeBtn"() {
+    $("body").toggleClass("dark");
+    if (document.body.classList.contains("dark")) {
+      document.getElementById("dlModeIcon").innerHTML = "light_mode";
+    } else {
+      document.getElementById("dlModeIcon").innerHTML = "dark_mode";
+    }
   },
 });

90f34e4acf5521e9484c78fa57967d47dd9fe828
Guillaume Darbonne <guidouil@gmail.com>
2023-05-09 14:17:59 +0200

    Fart counter

--- client/main.js
+++ client/main.js
@@ -1,14 +1,26 @@
 import { Template } from "meteor/templating";
+import { ReactiveVar } from "meteor/reactive-var";
 
 import "./main.html";
 
-const getRandomInt = () => Math.floor(Math.random() * 8) + 1;
+Template.home.onCreated(() => {
+  const instance = Template.instance();
+  instance.counter = new ReactiveVar(0);
+});
+
+Template.home.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
 
 Template.home.events({
-  "click #jaiPtBtn"() {
+  "click #jaiPtBtn"(event, templateInstance) {
+    const getRandomInt = () => Math.floor(Math.random() * 8) + 1;
     const num = getRandomInt();
     const audio = new Audio(`/snd/fart-0${num}.mp3`);
     audio.play();
+    templateInstance.counter.set(templateInstance.counter.get() + 1);
   },
 });
 
